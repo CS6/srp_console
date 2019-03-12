fb27573c90cc3e51530928cea17ed559f5cb14ec
Chih Hui Kao <kchforwork@gmail.com>
2019-03-12 20:51:21 +0800

    加上兩個半天確認

--- app/page/Request/Request.js
+++ app/page/Request/Request.js
@@ -10,7 +10,8 @@ import {
   Button,
   Picker,
   TouchableOpacity,
-  Modal
+  Modal,
+  Alert
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { Dropdown } from 'react-native-material-dropdown';
@@ -22,6 +23,9 @@ import moment from "moment";
 ///此為請假頁面
 // 取得屏幕的宽高Dimensions
 const { width, height } = Dimensions.get('window');
+
+const millisecondsFor12hours = 43200000;
+const millisecondsFor11h59min = 86399000;
 
 const leave_type = [
   {
@@ -70,12 +74,15 @@ class ApplyLeaveForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isChecked: false,
+      isCheckedFirstHalf: false,
+      isCheckedSecondHalf: false,
       selectedStartDate: null,
       selectedEndDate: null,
       selectedLeaveType: null,
       startModalVisible: false,
       endModalVisible: false,
+      issuer: "778TIlaNHBcW1lwvk3dZ1HuTuPv1",
+      leaveReason: null,
     };
     this.onStartDateChange = this.onStartDateChange.bind(this);
     this.onEndDateChange = this.onEndDateChange.bind(this);
@@ -102,15 +109,41 @@ class ApplyLeaveForm extends React.Component {
     this.setState({endModalVisible: visible});
   }
 
-  _numberOfDay(selectedEndDate, selectedStartDate, selectedLeaveType, isChecked){
-    if(selectedLeaveType == null) alert("請選擇假別");
-    else if(selectedStartDate == null) alert("請選擇起始日期");
-    else if(selectedEndDate == null) alert("請選擇終止日期");
-    else if(selectedEndDate < selectedStartDate) alert("終止日期不能比起始日期早");
+  onPost = (selectedStartDate,selectedEndDate,selectedLeaveType,leaveReason) =>{
+    fetch('https://us-central1-my-fuck-awesome-project.cloudfunctions.net/askLeave', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({
+        "issuer": this.state.issuer,
+        "type":selectedLeaveType, 
+        "startLeaveTime": Number(selectedStartDate), 
+        "endLeaveTime": Number(selectedEndDate),
+        "desc": leaveReason,
+      })
+    }).then((response) => response.json())
+      .then((data) =>  console.warn(data))
+      .catch((err)=>console.warn(err));
+  }
+
+  _numberOfDay(selectedEndDate, selectedStartDate, selectedLeaveType, leaveReason, isCheckedFirstHalf, isCheckedSecondHalf){
+    if(selectedLeaveType == null) Alert.alert("請假失敗","請選擇假別");
+    else if(selectedStartDate == null) Alert.alert("請假失敗","請選擇起始日期");
+    else if(selectedEndDate == null) Alert.alert("請假失敗","請選擇終止日期");
+    else if(selectedEndDate < selectedStartDate) Alert.alert("請假失敗","終止日期不能比起始日期早");
     else {
-      let numberOfDay = ((selectedEndDate-selectedStartDate)/1000/3600/24)+1;
-      if(isChecked == true) numberOfDay -= 0.5;
-      alert ("共請"+numberOfDay+"天假");
+      // let numberOfDay = ((selectedEndDate-selectedStartDate)/1000/3600/24)+1;
+      // if(isChecked == true) numberOfDay -= 0.5;
+      // console.warn(selectedStartDate,selectedEndDate);
+      // Alert.alert ("請假成功","共請"+numberOfDay+"天假");
+
+      if(isCheckedFirstHalf) selectedStartDate += millisecondsFor12hours;
+      if(isCheckedSecondHalf) selectedEndDate += millisecondsFor12hours;
+      else selectedEndDate += millisecondsFor11h59min;
+
+      this.onPost(selectedStartDate,selectedEndDate,selectedLeaveType,leaveReason);
+      Alert.alert ("請假成功");
     }
   }
 
@@ -120,25 +153,29 @@ class ApplyLeaveForm extends React.Component {
     });
   }
 
+  
+  // onGet() {
+  //   fetch('https://us-central1-my-fuck-awesome-project.cloudfunctions.net/getAnnouncement', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   }).then((response) => response.json())
+  //     .then((jsonData) => {
+  //       let announcement = jsonData.announcement;
+  //       alert("announcement:" + announcement.time._seconds);
+  //     });
+  // }
 
-  onGet() {
-    fetch('https://us-central1-my-fuck-awesome-project.cloudfunctions.net/getAnnouncement', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then((response) => response.json())
-      .then((jsonData) => {
-        let announcement = jsonData.announcement;
-        alert("announcement:" + announcement.time._seconds);
-      });
-  }
+
 
   renderApplyLeaveForm() {
     const selectedStartDate  = this.state.selectedStartDate;
     const selectedEndDate  = this.state.selectedEndDate;
     const selectedLeaveType  = this.state.selectedLeaveType;
-    let isChecked = this.state.isChecked;
+    let isCheckedFirstHalf = this.state.isCheckedFirstHalf;
+    let isCheckedSecondHalf = this.state.isCheckedSecondHalf;
+    let leaveReason = this.state.leaveReason;
     const startDate = selectedStartDate ? moment(selectedStartDate).format("YYYY/MM/DD") : '';
     const endDate = selectedEndDate ? moment(selectedEndDate).format("YYYY/MM/DD") : '';
     return (
@@ -163,19 +200,32 @@ class ApplyLeaveForm extends React.Component {
               <Text style={styles.dateClickText} >起始日期: <Text style={styles.dateSelectedText}>{startDate}</Text></Text>
             </TouchableOpacity>
 
+            <CheckBox
+              style={styles.halfDayCheckbox}
+              onClick={() => {
+                this.setState({
+                  isCheckedFirstHalf: !isCheckedFirstHalf
+                })
+              }}
+              isChecked={isCheckedFirstHalf}
+              rightText={"請上半天假"}
+              rightTextStyle={{color:'#808080',fontSize: 15,}}
+              checkBoxColor='#808080'
+            />
+
             <TouchableOpacity title='終止日期' style={styles.dateClick} onPress={() => { this.setEndModalVisible(!this.state.endModalVisible); }}>
               <Text style={styles.dateClickText} >終止日期: <Text style={styles.dateSelectedText}>{endDate}</Text></Text>
             </TouchableOpacity>
 
             <CheckBox
-              style={{ flex: 1, marginTop:20}}
+              style={styles.halfDayCheckbox}
               onClick={() => {
                 this.setState({
-                  isChecked: !isChecked
+                  isCheckedSecondHalf: !isCheckedSecondHalf
                 })
               }}
-              isChecked={isChecked}
-              rightText={"有一天為半天假"}
+              isChecked={isCheckedSecondHalf}
+              rightText={"請下半天假"}
               rightTextStyle={{color:'#808080',fontSize: 15,}}
               checkBoxColor='#808080'
             />
@@ -183,23 +233,23 @@ class ApplyLeaveForm extends React.Component {
             <TextInput
               style={styles.leaveModalTextbox}
               placeholder="請假理由"
-              // onChangeText={(txt) => { this.setState({ key: txt }) }}
-              value={this.state.key} />
+              onChangeText={(txt) => { this.setState({ leaveReason: txt }) }}
+              value={leaveReason} />
 
           </View>
           <View style={styles.leaveSubmit}>
             <TouchableOpacity title='送出假單' 
               style={styles.leaveSubmitBtn} 
-              onPress={() => {this._numberOfDay(selectedEndDate,selectedStartDate,selectedLeaveType,isChecked)}}>
+              onPress={() => {this._numberOfDay(selectedEndDate,selectedStartDate,selectedLeaveType,leaveReason,isCheckedFirstHalf,isCheckedSecondHalf)}}>
               <Text style={styles.leaveSubmitText} >送出假單</Text>
             </TouchableOpacity>
           </View>
           
 
-          <Button
+          {/* <Button
             onPress={() => { this.onGet() }}
             title="press"
-          />
+          /> */}
 
         </View>
         <Modal 
@@ -337,7 +387,7 @@ const styles = StyleSheet.create({
     // marginTop: 100,
   },
   dateClick:{
-    marginVertical: 10,
+    marginVertical: 15,
     borderBottomWidth: 0.4,
     borderBottomColor: '#8D8D8D'
   },
@@ -349,6 +399,11 @@ const styles = StyleSheet.create({
   dateSelectedText:{
     color: '#333',
   },
+  halfDayCheckbox:{
+    flex: 1,
+    marginBottom:10
+  },
+
   leaveSubmit:{
     flex:1,
     justifyContent:'center',
